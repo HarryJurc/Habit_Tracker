e0772cac66bfba128d47a056953695a2e93a6bac
Igor Gurchenko <131685738+HarryJurc@users.noreply.github.com>
2025-08-11 19:03:10 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,13 +95,14 @@ def clean_env_var(var):
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": clean_env_var(os.getenv("NAME"), "test_db"),
-        "USER": clean_env_var(os.getenv("USER"), "postgres"),
-        "PASSWORD": clean_env_var(os.getenv("PASSWORD"), "12345"),
+        "NAME": clean_env_var(os.getenv("NAME")) or "test_db",
+        "USER": clean_env_var(os.getenv("USER")) or "postgres",
+        "PASSWORD": clean_env_var(os.getenv("PASSWORD")) or "12345",
         "HOST": "localhost",
         "PORT": "5432",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
